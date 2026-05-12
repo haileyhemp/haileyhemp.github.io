2ae93327f1a75833aa1b8b838f23e4bdb4bf7648
haileyhemp.github.io <hemph0708@gmail.com>
2026-05-12 13:10:33 -0500

    lkjh

---
+++ javascriptstuff/login/scripts.js
@@ -0,0 +1,17 @@
+username_entry = document.getElementById("username");
+password_entry = document.getElementById("password");
+submit_button = document.getElementById("submit");
+status_text = document.getElementById("status-text");
+
+function checkCredentials(){
+    if (username_entry.value == "stitchlover07" && password_entry.value == "liloandstitch"){
+        status_text.innerHTML = "ACCESS GRANTED";
+        status_text.style.color = "limegreen";
+    }
+    else {
+        status_tect.innterHTML = "ACCESS DENIED";
+        status_text.style.color = "red";
+    }
+
+}
+submit_button.addEventListener("click", checkCredentials);
